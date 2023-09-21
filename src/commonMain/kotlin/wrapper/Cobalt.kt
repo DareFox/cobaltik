cc5c12435a3b0b8d0a3e5e0f9dc25916fe27a608
DareFox <easymalink@gmail.com>
2023-09-21 13:53:58 +0300

    Cobalt: add kdoc

--- src/commonMain/kotlin/wrapper/Cobalt.kt
+++ src/commonMain/kotlin/wrapper/Cobalt.kt
@@ -6,17 +6,43 @@ import wrapper.builders.AudioRequestBuilder
 import wrapper.builders.CobaltRequestBuilder
 import wrapper.builders.RequestBuilder
 
+/**
+ * The `Cobalt` class provides a high-level API for making requests to a Cobalt server.
+ *
+ * @property serverUrl The base URL of the Cobalt server.
+ */
 class Cobalt(val serverUrl: String) {
     private val raw = CobaltRaw(serverUrl)
 
+    /**
+     * Sends a Cobalt request using a custom builder with specific configuration options.
+     *
+     * @param builder The request builder to customize the request parameters.
+     * @param setupOptions A lambda function that configures the builder with additional options.
+     * @return [A wrapped Cobalt response][WrappedCobaltResponse].
+     */
     suspend fun <T> request(builder: RequestBuilder<T>, setupOptions: T.() -> Unit): WrappedCobaltResponse {
         return requestWithBuilder(builder, setupOptions)
     }
+
+    /**
+     * Sends a Cobalt request using a URL and optional configuration options.
+     *
+     * @param url The URL for the Cobalt request.
+     * @param setupOptions A lambda function that configures the request options if provided.
+     * @return [A wrapped Cobalt response][WrappedCobaltResponse].
+     */
     suspend fun request(
         url: String,
         setupOptions: (CobaltRequestBuilder.() -> Unit)? = null
     ): WrappedCobaltResponse = requestWithBuilder(CobaltRequestBuilder(url), setupOptions)
 
+    /**
+     * Sends a Cobalt request using a pre-built CobaltRequest object.
+     *
+     * @param requestObj The pre-built CobaltRequest object.
+     * @return [A wrapped Cobalt response][WrappedCobaltResponse].
+     */
     suspend fun request(requestObj: CobaltRequest): WrappedCobaltResponse = requestWrapped(requestObj)
 
     private suspend fun <T> requestWithBuilder(builder: RequestBuilder<T>, setupOptions: (T.() -> Unit)?): WrappedCobaltResponse {
