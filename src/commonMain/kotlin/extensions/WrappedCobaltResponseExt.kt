5e834e42b654309c0baca924bd34485489f3d69c
DareFox <easymalink@gmail.com>
2023-09-21 14:38:00 +0300

    WrappedCobaltResponse@Extensions: add kdoc

--- src/commonMain/kotlin/extensions/WrappedCobaltResponseExt.kt
+++ src/commonMain/kotlin/extensions/WrappedCobaltResponseExt.kt
@@ -4,6 +4,13 @@ import models.CobaltError
 import wrapper.ErrorResponse
 import wrapper.WrappedCobaltResponse
 
+/**
+ * Throws a [CobaltError] exception if this [WrappedCobaltResponse] represents [an error response][ErrorResponse].
+ * If the response is not an error, it returns the response itself.
+ *
+ * @return This [WrappedCobaltResponse] if it's not an [ErrorResponse]
+ * @throws CobaltError if wrapped response is [ErrorResponse].
+ */
 fun WrappedCobaltResponse.throwIfError(): WrappedCobaltResponse {
     if (this is ErrorResponse) {
         throw CobaltError(text ?: "[No error text]")
