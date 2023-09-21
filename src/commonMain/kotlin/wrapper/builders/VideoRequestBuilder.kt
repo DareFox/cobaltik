0fbb9276a819e9d830b695a16cf045174b185f4a
DareFox <easymalink@gmail.com>
2023-09-21 13:59:03 +0300

    VideoRequestBuilder: add Kdoc

--- src/commonMain/kotlin/wrapper/builders/VideoRequestBuilder.kt
+++ src/commonMain/kotlin/wrapper/builders/VideoRequestBuilder.kt
@@ -5,8 +5,18 @@ import models.CobaltRequest
 import models.VideoCodec
 import models.VideoQuality
 
+/**
+ * The `VideoRequestBuilder` class is used to construct Cobalt generic video requests with specific configuration options.
+ *
+ * @param url The URL for the video request.
+ *
+ * @see YouTubeRequestBuilder For creating specific YouTube requests
+ * @see TikTokRequestBuilder For creating specific TikTok requests
+ */
 class VideoRequestBuilder(val url: String): RequestBuilder<VideoRequestBuilder> {
+    /** The video quality to use for the request. 720 quality is recommended for phones. Defaults to [VideoQuality._720p]  **/
     var videoQuality = VideoQuality._720p
+    /** Disables audio track in video downloads. Defaults to `false` **/
     var muteAudio = false
 
     override fun build() = CobaltRequestBuilder(url).build {
