27384cf5c6d63393940a89870164a5f4a1a8d157
DareFox <easymalink@gmail.com>
2023-09-21 14:15:30 +0300

    WrappedCobaltResponse: make children serialiazble

--- src/commonMain/kotlin/wrapper/WrappedCobaltResponse.kt
+++ src/commonMain/kotlin/wrapper/WrappedCobaltResponse.kt
@@ -7,11 +7,17 @@ import models.PickerType
 @Serializable
 sealed class WrappedCobaltResponse
 
+@Serializable
 data class ErrorResponse(val text: String?): WrappedCobaltResponse()
+@Serializable
 data class RateLimitResponse(val text: String?): WrappedCobaltResponse()
+@Serializable
 data class SuccessResponse(val text: String?): WrappedCobaltResponse()
+@Serializable
 data class RedirectResponse(val redirectUrl: String): WrappedCobaltResponse()
+@Serializable
 data class StreamResponse(val streamUrl: String): WrappedCobaltResponse()
+@Serializable
 data class PickerResponse(
     val type: PickerType,
     val items: List<PickerItem>,
