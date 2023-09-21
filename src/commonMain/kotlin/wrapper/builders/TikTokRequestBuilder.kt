e175a453665e3e9d4860ddb7659094632a8c2fb6
DareFox <easymalink@gmail.com>
2023-09-21 13:56:44 +0300

    TikTokRequestBuilder: add kdoc

--- src/commonMain/kotlin/wrapper/builders/TikTokRequestBuilder.kt
+++ src/commonMain/kotlin/wrapper/builders/TikTokRequestBuilder.kt
@@ -4,10 +4,19 @@ import models.CobaltRequest
 import models.VideoCodec
 import models.VideoQuality
 
+/**
+ * Builder class for constructing TikTok video download requests with customizable options.
+ *
+ * @property url The URL of the TikTok video.
+ */
 class TikTokRequestBuilder(val url: String): RequestBuilder<TikTokRequestBuilder> {
+    /** Enables download of original sound used in a TikTok video. Defaults to `false` **/
     var downloadFullTikTokAudio = false
+    /** The video quality to use for the request. 720 quality is recommended for phones. Defaults to [VideoQuality._720p]  **/
     var videoQuality = VideoQuality._720p
+    /** Disables audio track in video downloads. Defaults to `false` **/
     var muteAudio = false
+    /** Changes whether downloaded TikTok videos have watermarks. Defaults to `false` **/
     var removeTikTokWatermark = false
 
     override fun build() = CobaltRequestBuilder(url).build {
