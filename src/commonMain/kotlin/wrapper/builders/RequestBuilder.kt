14ae66ae9cd86319b10a7f9d4dc70a4333edc032
DareFox <easymalink@gmail.com>
2023-09-21 13:48:32 +0300

    RequestBuilder: add KDoc to interface

--- src/commonMain/kotlin/wrapper/builders/RequestBuilder.kt
+++ src/commonMain/kotlin/wrapper/builders/RequestBuilder.kt
@@ -2,7 +2,24 @@ package wrapper.builders
 
 import models.CobaltRequest
 
+/**
+ * An interface for building Cobalt requests. It allows the construction of CobaltRequest objects
+ * with various configuration options.
+ *
+ * @param T The specific builder implementation type.
+ */
 interface RequestBuilder<T> {
+    /**
+     * Builds and returns a CobaltRequest with the current builder's configuration.
+     *
+     * @return A CobaltRequest with the configured parameters.
+     */
     fun build(): CobaltRequest
+    /**
+     * Builds a CobaltRequest by applying the given lambda function to the current builder.
+     *
+     * @param func A lambda function that configures the builder.
+     * @return A CobaltRequest with the configured parameters.
+     */
     fun build(func: T.() -> Unit): CobaltRequest
 }
