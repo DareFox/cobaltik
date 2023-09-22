d2c9f7e7410a5ca18a6fea721fe51594adb0657c
DareFox <easymalink@gmail.com>
2023-09-22 12:47:25 +0300

    CobaltResponseExt@Test: create test

---
+++ src/commonTest/kotlin/extensions/CobaltResponseExt.kt
@@ -0,0 +1,44 @@
+package extensions
+
+import models.CobaltError
+import models.CobaltResponse
+import models.CobaltResponseStatus
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class CobaltResponseExtensionsTest {
+
+    @Test
+    fun testIsError() {
+        // Create a CobaltResponse representing an error
+        val errorResponse = CobaltResponse(CobaltResponseStatus.ERROR, text = "An error occurred")
+
+        // Create a CobaltResponse representing success
+        val successResponse = CobaltResponse(CobaltResponseStatus.SUCCESS)
+
+        // Check if isError correctly identifies error responses
+        assertTrue(errorResponse.isError())
+        assertFalse(successResponse.isError())
+    }
+
+    @Test
+    fun testThrowIfError() {
+        // Create a CobaltResponse representing an error
+        val errorResponse = CobaltResponse(CobaltResponseStatus.ERROR, text = "An error occurred")
+
+        // Create a CobaltResponse representing success
+        val successResponse = CobaltResponse(CobaltResponseStatus.SUCCESS)
+
+        // Test that throwIfError throws an exception for error responses
+        assertFailsWith<CobaltError> {
+            errorResponse.throwIfError()
+        }
+
+        // Test that throwIfError returns the response for success responses
+        val returnedResponse = successResponse.throwIfError()
+        assertEquals(successResponse, returnedResponse)
+    }
+}
