ec48f98e984cadba9817ad78e2e8b1d55245e11a
DareFox <easymalink@gmail.com>
2023-09-27 13:55:12 +0300

    Gradle@detekt: enable reporting

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,16 @@ detekt {
     config.setFrom("$projectDir/config/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
 }
 
+tasks.withType<Detekt>().configureEach {
+    reports {
+        html.required.set(true) // observe findings in your browser with structure and code snippets
+        xml.required.set(true) // checkstyle like format mainly for integrations like Jenkins
+        txt.required.set(true) // similar to the console output, contains issue signature to manually edit baseline files
+        sarif.required.set(true) // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations with GitHub Code Scanning
+        md.required.set(true) // simple Markdown format
+    }
+}
+
 val ktorVersion = "2.2.4"
 
 kotlin {
