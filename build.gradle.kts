173636c98b544f66d043af7e35ce19c1741c8e27
DareFox <easymalink@gmail.com>
2023-09-27 12:59:22 +0300

    Gradle@JS: Add tests support for browser

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,19 @@ repositories {
 val ktorVersion = "2.2.4"
 
 kotlin {
+    val hostOs = System.getProperty("os.name")
+    val isArm64 = System.getProperty("os.arch") == "aarch64"
+    val isMingwX64 = hostOs.startsWith("Windows")
+
+    val nativeTarget = when {
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
     jvm {
         jvmToolchain(8)
         withJava()
@@ -25,25 +38,18 @@ kotlin {
     js {
         browser {
             generateTypeScriptDefinitions()
+            testTask(Action {
+                useKarma {
+                    useFirefoxHeadless()
+                    if (hostOs == "Mac OS X") useSafari()
                 }
+            })
         }
         nodejs() {
             generateTypeScriptDefinitions()
         }
     }
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
 
-    
     sourceSets {
         val commonMain by getting {
             dependencies {
