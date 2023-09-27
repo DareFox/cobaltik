9cf9f46714e04ad9a7c38bd150957abb28053466
DareFox <easymalink@gmail.com>
2023-09-27 14:27:16 +0300

    Ktor: extract seconds of inactivity to constant variable

--- src/commonMain/kotlin/Ktor.kt
+++ src/commonMain/kotlin/Ktor.kt
@@ -6,8 +6,11 @@ import kotlinx.serialization.json.Json
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
+
+private const val MAX_SECONDS_OF_INACTIVITY = 30
+
 @OptIn(ExperimentalSerializationApi::class)
-internal val ktor = LifecycleKtor((30).toDuration(DurationUnit.SECONDS)) {
+internal val ktor = LifecycleKtor(MAX_SECONDS_OF_INACTIVITY.toDuration(DurationUnit.SECONDS)) {
     HttpClient() {
         install(ContentNegotiation) {
             json(Json {
