4b879e235cf4facb5f97eb9fb0b82467380df409
DareFox <easymalink@gmail.com>
2023-09-27 13:18:05 +0300

    Gradle@Kotlin: add kotlin static analyzer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("multiplatform") version "1.9.10"
     kotlin("plugin.serialization") version "1.9.10"
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
     id("maven-publish")
 }
 
