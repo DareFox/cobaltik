2b0b1a6a17f88ee7af5336d81dcd98309c212c70
DareFox <easymalink@gmail.com>
2023-10-23 04:25:54 +0300

    buildSrc@Gradle: specify kotlin and android tools versions in variable

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,7 +6,10 @@ repositories.gradlePluginPortal()
 repositories.google()
 repositories.mavenCentral()
 
+val KOTLIN_VER = "1.9.10"
+val ANDROD_TOOLS_VER = "8.1.0"
+
 dependencies {
-    implementation("org.jetbrains.kotlin.multiplatform:org.jetbrains.kotlin.multiplatform.gradle.plugin:1.9.10")
-    implementation("com.android.tools.build:gradle:8.1.0")
+    implementation("org.jetbrains.kotlin.multiplatform:org.jetbrains.kotlin.multiplatform.gradle.plugin:$KOTLIN_VER")
+    implementation("com.android.tools.build:gradle:$ANDROD_TOOLS_VER")
 }
