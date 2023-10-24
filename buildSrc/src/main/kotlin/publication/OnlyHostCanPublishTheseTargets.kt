fd15691b899a009785ea3c110a135af4ba0ad8bf
DareFox <easymalink@gmail.com>
2023-10-24 16:50:31 +0300

    buildSrc@Gradle: skip publishing tasks with its dependecies
    Because takeIf skips task, but not dependecies for this task
    https://discuss.gradle.org/t/how-to-skip-compilation-for-some-kmm-targets/46842

--- buildSrc/src/main/kotlin/publication/OnlyHostCanPublishTheseTargets.kt
+++ buildSrc/src/main/kotlin/publication/OnlyHostCanPublishTheseTargets.kt
@@ -7,28 +7,19 @@ import org.gradle.kotlin.dsl.withType
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskContainer
 import host.currentMachine
+import org.gradle.kotlin.dsl.support.uppercaseFirstChar
 
-fun PublicationContainer.onlyHostCanPublishTheseTargets(
-    machine: Machine,
-    targets: List<String>,
-    tasks: TaskContainer
+fun Project.onlyHostCanPublishTheseTargets(
+    publishingMachine: Machine,
+    target: List<String>
 ) {
-    matching { it.name in targets }.all {
-        val targetPublication = this@all
+    val excludedTasks = target.map(::getPublishTaskName)
 
-        tasks.withType<AbstractPublishToMaven>()
-            .matching { (it.publication == targetPublication) }
-            .configureEach {
-                onlyIf {
-                    val canPublish = currentMachine == machine
-                    when(canPublish) {
-                        true ->
-                            println("Current host ($machine) can publish ${targetPublication.name} target")
-                        false ->
-                            println("Only $machine can publish ${targetPublication.name} target")
-                    }
-                    canPublish
-                }
-            }
+    if (publishingMachine != currentMachine) {
+        println("Excluded tasks $excludedTasks because $currentMachine can't publish those targets")
+        gradle.startParameter.excludedTaskNames.addAll(excludedTasks)
     }
 }
+
+private fun getPublishTaskName(target: String): String
+    = "publish${target.uppercaseFirstChar()}ToMavenLocal"
