cf74034b14730590a66657d4d2f0a772a481c60c
DareFox <easymalink@gmail.com>
2023-11-16 14:53:02 +0300

    build: Crosscompile x86 and arm on Linux host

--- buildSrc/src/main/kotlin/multiplatform/SetupNativeTargetsFor.kt
+++ buildSrc/src/main/kotlin/multiplatform/SetupNativeTargetsFor.kt
@@ -8,10 +8,10 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 fun KotlinMultiplatformExtension.setupNativeTargetsFor(machine: Machine): List<KotlinNativeTarget> {
     return when(machine) {
-        Machine(OS.Linux, Arch.Arm) -> linuxTargetsArm()
-        Machine(OS.Linux, Arch.X86) -> linuxTargetsX86()
         Machine(OS.MacOS, Arch.Arm),
         Machine(OS.MacOS, Arch.X86) -> darwinTargetsX86() + darwinTargetsArm()
+        Machine(OS.Linux, Arch.Arm),
+        Machine(OS.Linux, Arch.X86) -> linuxTargetsX86() + linuxTargetsArm()
         Machine(OS.Windows, Arch.X86) -> windowsTargetsX86()
         else -> listOf()
     }
